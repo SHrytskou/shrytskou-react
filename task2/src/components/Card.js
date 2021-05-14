3fa8c63dc99be1933946cddee15da14587d2c18c
SHrytskou <SHrytskou@gomel.iba.by>
2021-05-14 21:32:44 +0300

    task#2

---
+++ task2/src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+
+function Card() {
+  return (
+		<div className="card">
+			<h3 className="caption">Card Title</h3>
+			<hr></hr>
+			<p>Some Text</p>
+		</div>
+	);
+}
+
+export default Card;
