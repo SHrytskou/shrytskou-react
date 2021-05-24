4baf67c24c7b1c1f7f4665efcb6c4f011e778d2d
SHrytskou <SHrytskou@gomel.iba.by>
2021-05-24 11:48:25 +0300

    task#3

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,42 @@
+import React from "react";
+
 import './Card.css'
 
-function Card() {
-  return (
-		<div className="card">
-			<h3 className="caption">Card Title</h3>
-			<hr></hr>
-			<p>Some Text</p>
-		</div>
-	);
+class Card extends React.Component {
+	constructor(props) {
+	  super(props)
+	  this.output = this.output.bind(this);
+	  this.state = {
+		divName: 'card'
+	  }
+	}
+	
+	output() {
+	  if(this.state.divName === 'card'){
+		this.setState({divName: 'card-item'});
+	  }
+	  if(this.state.divName === 'card-item'){
+		this.setState({divName: 'card'});
+	  }
+	  
+	}
+   
+	render() {
+	  return(
+		<div className={this.state.divName}>
+		 <div className={'card-inner__caption'}>
+		   <h3 className="card-caption">Card Title</h3>
+		   <input type="checkbox" onChange={this.output} />
+		 </div>
+  
+		 <hr></hr>
+  
+		 <div>
+		   <p>Some Text</p>
+		 </div>
+	   </div>
+	  )
+	}  
 }
 
 export default Card;
